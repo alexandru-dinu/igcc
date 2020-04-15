1fcbe99246776b25a98ffb5ee5889793388c0a75
Alexandru Dinu <alex.dinu07@gmail.com>
2020-04-15 18:17:19 +0800

    [source] more default includes

--- libigcc/source_code.py
+++ libigcc/source_code.py
@@ -1,7 +1,29 @@
 file_boilerplate = """
-#include <cstdio>
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
 
 $user_includes
 
