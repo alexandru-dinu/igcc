58bbd8cc1c8c66b25351e2d6561826f3381da64a
Alexandru Dinu <alex.dinu07@gmail.com>
2020-04-21 05:55:32 +0800

    Simpler boilerplate.

--- libigcc/boilerplate.h
+++ libigcc/boilerplate.h
@@ -1,28 +1,5 @@
-#include <algorithm>
-#include <assert.h>
-#include <bitset>
-#include <deque>
-#include <functional>
 #include <iostream>
-#include <iterator>
-#include <limits>
-#include <list>
-#include <map>
-#include <math.h>
-#include <numeric>
-#include <queue>
-#include <set>
-#include <sstream>
-#include <stack>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <string>
-#include <time.h>
-#include <unordered_map>
-#include <unordered_set>
-#include <utility>
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
